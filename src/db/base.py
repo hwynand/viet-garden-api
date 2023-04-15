2af20ea7c7266d5416302be9ede71fd4349d26c4
huongpx <huong.px01@gmail.com>
2023-04-15 22:33:01 +0700

    add title model and migration

--- src/db/base.py
+++ src/db/base.py
@@ -2,6 +2,7 @@
 # and auto generate migration
 # https://stackoverflow.com/questions/15660676/alembic-autogenerate-producing-empty-migration
 from db.base_model import Base
+from models.article import Article
 from models.cart import CartProduct
 from models.category import Category, CategoryGroup
 from models.order import Order, OrderProduct
